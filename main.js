ff2548cecaeeeaa65407f1331c539a05b2d759c7
itotallyrock <itotallyrock@users.noreply.github.com>
2015-02-08 22:33:10 -0600

    Added Confirm and Alert while fixing notfiy

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 plugInterface = {
 	notify: function(i,m){
-		require("b20d6/f1e58/e027b").trigger("notify",i,m);
+		require("d56b1/c7e48/a239e").trigger("notify",i,m)
+	},
+	alert: function(t,b){
+		require(["d56b1/c7e48/a239e","d56b1/adf9d/a28a5"],function(s,o){
+			s.dispatch(new o(o.ALERT,t,b));
+		});
+	},
+	confirm: function(t,b){//NO RETURN
+		require(["d56b1/c7e48/a239e","d56b1/adf9d/a28a5"],function(s,o){
+			s.dispatch(new o(o.CONFIRM,t,b));
+		});
 	}
 }
