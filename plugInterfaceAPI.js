10a8ba84afba27f436fb3bc2ff89369428f885df
itotallyrock <itotallyrock@users.noreply.github.com>
2015-02-15 01:43:52 -0600

    Added Chat Types and Bug Fix

--- plugInterfaceAPI.js
+++ plugInterfaceAPI.js
@@ -1,14 +1,25 @@
 var plugInterface = {
-	_e: require("d56b1/c7e48/a239e"),
-	_o: require("d56b1/adf9d/a28a5"),
+	_e: require("e4881/bc229/d5ab0"),
+	_o: require("e4881/bac6a/f5cc1"),
+	_chattypes: {
+		"":"",
+		alert:"c42e3b;",
+		system:"c42e3b;",
+		error:"c42e3b;",
+		info:"009cdd;",
+		success:"90ad2f;",
+		valid:"90ad2f;",
+		warn:"ffdd6f",
+		"default":"ac76ff"
+	},
 	chat: function(t,m,n,b,i){
 		if(!m){return}
 		if(typeof m !== "string"){m = m.html();}
-		$("#chat-messages").append("<div class=\"cm message\" ><div class=\"badge-box "+(!b ? "no-badge" : "")+"\"><i class=\""+(!b ? "" : "bdg")+" "+(!b ? "" : b)+"\"></i></div><div class=\"msg\"><div class=\"from\">"+(i ? ("<i class=\"icon "+i+"\"></i>") : "")+"<span class=\"un clickable\">"+n+"</span><span class=\"timestamp\" style=\"display: inline;\">"+(new Date().getHours()%12)+":"+(new Date().getMinutes()<10?"0":"")+new Date().getMinutes()+(new Date().getHours()%12>0?"pm":"am")+"</span></div><div class=\"text\">"+m+"</div></div></div>");
+		$("#chat-messages").append("<div class=\"cm message\" style=\"border-bottom:3px solid #"+this._chattypes[t]+"\"><div style=\"border:2px solid #"+this._chattypes[t]+"\"class=\"badge-box "+(!b ? "no-badge" : "")+"\"><i class=\""+(!b ? "" : "bdg")+" "+(!b ? "" : b)+"\"></i></div><div class=\"msg\"><div class=\"from\">"+(i ? ("<i class=\"icon "+i+"\"></i>") : "")+"<span class=\"un clickable\" style=\"color:#"+this._chattypes[t]+"\">"+n+"</span><span class=\"timestamp\" style=\"display: inline;\">"+(new Date().getHours()%12)+":"+(new Date().getMinutes()<10?"0":"")+new Date().getMinutes()+(new Date().getHours()%12>0?"pm":"am")+"</span></div><div class=\"text\">"+m+"</div></div></div>");
 		
 	},
 	notify: function(i,m){
-		require("d56b1/c7e48/a239e").trigger("notify",i,m);
+		_e.trigger("notify",i,m);
 	},
 	alert: function(t,b){
 		plugInterface._e.dispatch(new _o(_o.ALERT,t,b));
