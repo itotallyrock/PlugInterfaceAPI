6e3f3a33739e1989c6fa1b27c70b73e21df209fc
itotallyrock <itotallyrock@users.noreply.github.com>
2015-08-14 21:31:04 -0500

    Another Temp Fix
    Some slightly better looking chat styles

--- plugInterfaceAPI.js
+++ plugInterfaceAPI.js
@@ -1,6 +1,6 @@
 var plugInterface = {
-	_e: require("f06e2/edaf9/b7093"),
-	_o: require("f06e2/d391a/b2a65"),
+	_e: require("a804c/d3c3d/f4ad2"),
+	_o: "";//require("f06e2/d391a/b2a65"),
 	_chattypes: {
 		"":"",
 		alert:"c42e3b;",
@@ -11,12 +11,12 @@ var plugInterface = {
 		success:"73E232;",
 		valid:"90ad2f;",
 		warn:"ffdd6f",
-		"default":"ac76ff"
+		staff:"ac76ff"
 	},
 	chat: function(t,m,n,b,i,c){
 		if(!m){return}
 		if(typeof m !== "string"){m = m.html();}
-		$("#chat-messages").append("<div class=\"cm message\" style=\"border-bottom:3px solid #"+this._chattypes[t]+"\"><div style=\"border:2px solid #"+this._chattypes[t]+"\"class=\"badge-box "+(!b ? "no-badge" : "")+"\"><i class=\""+(!b ? "" : "bdg")+" "+(!b ? "" : b)+"\"></i></div><div class=\"msg\"><div class=\"from\">"+(i ? ("<i class=\"icon "+i+"\"></i>") : "")+"<span class=\"un clickable\" style=\"color:#"+(c ? c : this._chattypes[t])+"\">"+n+"</span><span class=\"timestamp\" style=\"display: inline;\">"+(new Date().getHours()%12)+":"+(new Date().getMinutes()<10?"0":"")+new Date().getMinutes()+(new Date().getHours()%12>0?"pm":"am")+"</span></div><div class=\"text\">"+m+"</div></div></div>");
+		$("#chat-messages").append("<div class=\"cm message\" style=\"border-left:3px solid #"+this._chattypes[t]+"\"><div class=\"badge-box "+(!b ? "no-badge" : "")+"\"><i class=\""+(!b ? "" : "bdg")+" "+(!b ? "" : b)+"\"></i></div><div class=\"msg\"><div class=\"from\">"+(i ? ("<i class=\"icon "+i+"\"></i>") : "")+"<span class=\"un clickable\" style=\"color:#"+(c ? c : this._chattypes[t])+"\">"+n+"</span><span class=\"timestamp\" style=\"display: inline;\">"+(new Date().getHours()%12)+":"+(new Date().getMinutes()<10?"0":"")+new Date().getMinutes()+(new Date().getHours()%12>0?"pm":"am")+"</span></div><div class=\"text\">"+m+"</div></div></div>");
 		$("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
 	},
 	notify: function(i,m){
